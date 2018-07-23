dc312a13f3816e71ba69f5daabd5d3199a45df95
BhavyansM <bhavyanshmishra@gmail.com>
2018-07-23 00:39:22 -0500

    Add the JSON, NetworkUtil and Model classes

--- app/src/main/java/com/computecrib/bakingapp/MainActivity.java
+++ app/src/main/java/com/computecrib/bakingapp/MainActivity.java
@@ -1,13 +1,86 @@
 package com.computecrib.bakingapp;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.computecrib.bakingapp.util.JsonUtilities;
+import com.computecrib.bakingapp.util.NetworkUtils;
+
+import org.json.JSONException;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_rest_data) TextView mRestDataTextView;
+
+    private ArrayList<Recipe> recipes;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        if(NetworkUtils.isConnected((ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE))){
+            new GetRecipeJSONTask().execute("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json");
+        }else{
+            Toast.makeText(this, R.string.network_unavailable_error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    @SuppressLint("StaticFieldLeak")
+    public class GetRecipeJSONTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String fullURL = urls[0];
+            String recipeJSONResults = null;
+            try {
+                recipeJSONResults = NetworkUtils.getBakingRecipesJSONString(fullURL);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return recipeJSONResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s!=null && !s.equals("")){
+                try {
+                    recipes = JsonUtilities.getRecipesFromJSON(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                String recs = "";
+                for(Recipe r : recipes)
+                    recs += r.getSteps().get(2).getShortDescription() + "\n";
+
+                mRestDataTextView.setText(recs);
+
+//                adapter.setMovies(movies);
+//                adapter.notifyDataSetChanged();
+//                if(recyclerSavedState != null){
+//                    grid.onRestoreInstanceState(recyclerSavedState);
+//                }
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/computecrib/bakingapp/util/NetworkUtils.java
+++ app/src/main/java/com/computecrib/bakingapp/util/NetworkUtils.java
@@ -1,8 +1,42 @@
 package com.computecrib.bakingapp.util;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.view.textclassifier.TextLinks;
+
+import java.io.IOException;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public final class NetworkUtils {
 
-    private static String getBakingRecipesJSONString(String url){
+    public static String getBakingRecipesJSONString(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
 
+        if (response.body() != null) {
+            return response.body().string();
+        }else{
+            return null;
+        }
     }
+
+    //Inspired by StackOverflow thread:
+    //https://stackoverflow.com/questions/4238921/detect-whether-there-is-an-internet-connection-available-on-android
+    //Date: 15 July 2018
+    public static boolean isConnected(ConnectivityManager cm) {
+        NetworkInfo networkStatus = null;
+        if (cm != null) {
+            networkStatus = cm.getActiveNetworkInfo();
+        }
+        return networkStatus != null && networkStatus.isConnected();
+    }
+
 }
